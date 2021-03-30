b26a4a801dab78aab96353640d2ee9259c08c0a6
Brian Casey <singulusoculus@gmail.com>
2021-03-30 10:04:38 -0500

    update about

--- src/routes/about.svelte
+++ src/routes/about.svelte
@@ -56,7 +56,7 @@
 		<div class="container">
 			<div class="content">
 				<p>Hi, I'm Brian Casey - husband, father of four, follower of Jesus. It's nice to meet you.</p>
-				<p>My main bread and butter is as a SQL Developer/Admin (SQL Server) and ERP specialist (Dynamics SL) for a mid-sized engineering and construction company but I'm also a web developer and web consultant. I come from a finance and project accounting background (my degree and original professional experience).</p>
+				<p>My main bread and butter is as a Data Analyst and SQL Developer/Admin (SQL Server) and ERP specialist (Dynamics SL) for a mid-sized engineering and construction company but I'm also a web developer and web consultant. I come from a finance and project accounting background (my degree and original professional experience).</p>
 				<p>I am a board gamer by hobby. Like any good hobby it made me want to create things for it which is what pushed me even further as a web developer. I love to play games like Carcassonne, Star Wars Rebellion, and Irish Gauge.</p>
 				<p>I love to watch good movies with my wife or get down on the floor and wrestle with my four boys. You will also find me reading, enjoying a good cup of coffee, listening to a podcast, drumming, doing weird exercises, writing, and trying to make my house a better home. I'm also short one eye thanks to a childhood cancer. I feel very blessed to be here.</p>
 			</div>
