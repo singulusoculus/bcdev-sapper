67ea3f0cf409affbf44bb86cac2bbb4194cbbb08
Brian Casey <singulusoculus@gmail.com>
2021-03-30 10:01:43 -0500

    fix

--- src/routes/contact.svelte
+++ src/routes/contact.svelte
@@ -25,7 +25,7 @@
 	<PageHeading heading={'Contact'} />
 
 	<PageContent {content} >
-		<p>Do you have a project you want me to work on? Want do hire me?</p>
+		<p>Do you have a project you want me to work on? Want to hire me?</p>
 		<p>Send me an email at briancasey@outlook.com with the details.</p> 
 	</PageContent>
 
